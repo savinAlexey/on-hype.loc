75764718f2748ff3a064d9a155655684e2f70221
savinAlexey <35041776+savinAlexey@users.noreply.github.com>
2025-04-04 16:18:02 +0800

    Update welcome.blade.php

--- views/welcome.blade.php
+++ views/welcome.blade.php
@@ -1,1 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<main class="container flex-grow-1 d-flex flex-column justify-content-center">
+    <p>Главная страница</p>
+</main>
+@endsection
+
+
 
