ad2a78ac0bda8f8ba9d7ff27e020df56cfcbf59c
savinAlexey <35041776+savinAlexey@users.noreply.github.com>
2025-04-04 16:22:28 +0800

    Update register.blade.php

--- views/auth/register.blade.php
+++ views/auth/register.blade.php
@@ -1,1 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-center align-items-center vh-100 vw-100">
+        <div class="w-100 p-4 bg-white shadow-sm" style="max-width: 28rem; border-radius: 0.5rem;">
+            <form method="POST"  action="{{ route('register') }}">
+                @csrf
+
+                <!-- Name -->
+                <div class="mb-3">
+                    <label for="name" class="form-label">{{ __('Name') }}</label>
+                    <input type="text" id="name" class="form-control" name="name" value="{{ old('name') }}" required autofocus autocomplete="name">
+                    @error('name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <label for="email" class="form-label">{{ __('Email') }}</label>
+                    <input type="email" id="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="username">
+                    @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <!-- Password -->
+                <div class="mb-3">
+                    <label for="password" class="form-label">{{ __('Password') }}</label>
+                    <input type="password" id="password" class="form-control" name="password" required autocomplete="new-password">
+                    @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <!-- Confirm Password -->
+                <div class="mb-4">
+                    <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
+                    <input type="password" id="password_confirmation" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    @error('password_confirmation')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <a class="small text-decoration-none" href="{{ route('login') }}">{{ __('Already registered?') }}</a>
+
+                    <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
 
