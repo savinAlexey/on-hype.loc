6a69f6b5e5cbdb2b012ee4d1deeb9a3b04b0880c
savinAlexey <35041776+savinAlexey@users.noreply.github.com>
2025-04-04 16:21:53 +0800

    Update login.blade.php

--- views/auth/login.blade.php
+++ views/auth/login.blade.php
@@ -1,1 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-center align-items-center vh-100 vw-100">
+        <div class="w-100 p-4 bg-white shadow-sm" style="max-width: 28rem; border-radius: 0.5rem;">
+            <!-- Session Status -->
+            <div class="mb-4">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+            </div>
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <label for="email" class="form-label">{{ __('Email') }}</label>
+                    <input type="email" id="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                    @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <!-- Password -->
+                <div class="mb-3">
+                    <label for="password" class="form-label">{{ __('Password') }}</label>
+                    <input type="password" id="password" class="form-control" name="password" required autocomplete="current-password">
+                    @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <!-- Remember Me -->
+                <div class="mb-3 form-check">
+                    <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+                    <label for="remember_me" class="form-check-label small text-muted">{{ __('Remember me') }}</label>
+                </div>
+
+                <div class="d-flex justify-content-between align-items-center">
+                    @if (Route::has('password.request'))
+                        <a class="small text-decoration-none" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                    @endif
+
+                    <button type="submit" class="btn btn-primary">{{ __('Log in') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
 
