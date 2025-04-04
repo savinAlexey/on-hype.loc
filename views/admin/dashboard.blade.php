c2d11123c8d4a06a67e020a5f0e2cdbc8f84d78e
savinAlexey <35041776+savinAlexey@users.noreply.github.com>
2025-04-04 16:18:54 +0800

    Update dashboard.blade.php

--- views/admin/dashboard.blade.php
+++ views/admin/dashboard.blade.php
@@ -1,1 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Страница Alex</h1>
+        <p>Доступно только администраторам</p>
+    </div>
+@endsection
 
