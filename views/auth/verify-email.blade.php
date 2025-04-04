e4902c06cb7d3ab100d201aefdb43f604252b23a
savinAlexey <35041776+savinAlexey@users.noreply.github.com>
2025-04-04 16:23:48 +0800

    Update verify-email.blade.php

--- views/auth/verify-email.blade.php
+++ views/auth/verify-email.blade.php
@@ -1,1 +1,30 @@
+<x-guest-layout>
+    <div class="alert alert-info mb-4 small">
+        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+    </div>
+
+    @if (session('status') == 'verification-link-sent')
+        <div class="alert alert-success mb-4 small">
+            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+        </div>
+    @endif
+
+    <div class="d-flex justify-content-between align-items-center mt-4">
+        <form method="POST" action="{{ route('verification.send') }}" class="mb-0">
+            @csrf
+            <div>
+                <x-primary-button class="btn btn-primary">
+                    {{ __('Resend Verification Email') }}
+                </x-primary-button>
+            </div>
+        </form>
+
+        <form method="POST" action="{{ route('logout') }}" class="mb-0">
+            @csrf
+            <button type="submit" class="btn btn-link text-decoration-none p-0">
+                {{ __('Log Out') }}
+            </button>
+        </form>
+    </div>
+</x-guest-layout>
 
