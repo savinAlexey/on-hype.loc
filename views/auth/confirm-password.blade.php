9027627a41240f519b406c694cdfd014acc846c4
savinAlexey <35041776+savinAlexey@users.noreply.github.com>
2025-04-04 16:20:38 +0800

    Update confirm-password.blade.php

--- views/auth/confirm-password.blade.php
+++ views/auth/confirm-password.blade.php
@@ -1,1 +1,26 @@
+<x-guest-layout>
+    <div class="alert alert-info mb-4">
+        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+    </div>
+
+    <form method="POST" action="{{ route('password.confirm') }}">
+        @csrf
+
+        <!-- Password -->
+        <div class="mb-3">
+            <x-input-label for="password" :value="__('Password')" />
+            <x-text-input id="password" class="form-control"
+                          type="password"
+                          name="password"
+                          required autocomplete="current-password" />
+            <x-input-error :messages="$errors->get('password')" class="invalid-feedback d-block" />
+        </div>
+
+        <div class="d-flex justify-content-end mt-4">
+            <x-primary-button class="btn btn-primary">
+                {{ __('Confirm') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
 
